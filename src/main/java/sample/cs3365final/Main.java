a7415ee1b78235454a516dbb1f1964cd8dab80cd
QGPike <69122709+QGPike@users.noreply.github.com>
2021-11-27 23:54:42 -0600

    commit

--- src/main/java/sample/cs3365final/Main.java
+++ src/main/java/sample/cs3365final/Main.java
@@ -29,10 +29,7 @@ public class Main extends Application{
         changeStage(stage,0);
     }
 
-    public void loyalCustomer()
-    {
 
-    }
 
     public void changeStage(Stage stage, int newStage) throws FileNotFoundException {
 
@@ -271,6 +268,122 @@ public class Main extends Application{
 
     }
 
+    public void loyalCustomer(int newStage) throws FileNotFoundException
+    {
+        try
+        {
+            isFirst = false;
+            changeStage(primaryStage,newStage); //changes to the next stage of origin
+
+            ///////////////DISPLAY/////////////////
+            stage.setTitle("Enter Item ID With the Numpad");
+            GridPane grid = new GridPane();
+            grid.setPadding(new Insets(25, 25, 25, 25));
+            grid.setVgap(18);
+            grid.setHgap(10);
+
+
+            Button Scale = new Button("Scale");
+            Scale.setStyle("-fx-background-color: MediumSeaGreen");
+            Scale.setMinSize(60, 45);
+            GridPane.setConstraints(Scale, 2, 3);
+            //Scan.setOnAction(e -> btn_Scale());
+            Button Enter = new Button("Enter:");
+            Enter.setStyle("-fx-background-color: MediumSeaGreen");
+            Enter.setMinSize(60, 45);
+            GridPane.setConstraints(Enter, 4, 3);
+            Enter.setOnAction(e -> {
+                try {
+                    btn_numpadEnter();
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+            });
+
+
+//---------------------------------------------------------------------numpad -->
+            Label lblNumpad = new Label("NUMPAD:");
+            GridPane.setConstraints(lblNumpad, 3, 9);
+            Label lblNumber = new Label(ItemID);
+            GridPane.setConstraints(lblNumber, 4,9 );
+
+
+            Button num1 = new Button("1");
+            num1.setStyle("-fx-background-color: MediumSeaGreen");
+            num1.setMinSize(60, 45);
+            GridPane.setConstraints(num1, 2, 5);
+            num1.setOnAction(e -> btn_Numpad(1));
+
+            Button num2 = new Button("2");
+            num2.setStyle("-fx-background-color: MediumSeaGreen");
+            num2.setMinSize(60, 45);
+            GridPane.setConstraints(num2, 3, 5);
+            num2.setOnAction(e -> btn_Numpad(2));
+
+            Button num3 = new Button("3");
+            num3.setStyle("-fx-background-color: MediumSeaGreen");
+            num3.setMinSize(60, 45);
+            GridPane.setConstraints(num3, 4, 5);
+            num3.setOnAction(e -> btn_Numpad(3));
+
+            Button num4 = new Button("4");
+            num4.setStyle("-fx-background-color: MediumSeaGreen");
+            num4.setMinSize(60, 45);
+            GridPane.setConstraints(num4, 2, 6);
+            num4.setOnAction(e -> btn_Numpad(4));
+
+            Button num5 = new Button("5");
+            num5.setStyle("-fx-background-color: MediumSeaGreen");
+            num5.setMinSize(60, 45);
+            GridPane.setConstraints(num5, 3, 6);
+            num5.setOnAction(e -> btn_Numpad(5));
+
+            Button num6 = new Button("6");
+            num6.setStyle("-fx-background-color: MediumSeaGreen");
+            num6.setMinSize(60, 45);
+            GridPane.setConstraints(num6, 4, 6);
+            num6.setOnAction(e -> btn_Numpad(6));
+
+            Button num7 = new Button("7");
+            num7.setStyle("-fx-background-color: MediumSeaGreen");
+            num7.setMinSize(60, 45);
+            GridPane.setConstraints(num7, 2, 7);
+            num7.setOnAction(e -> btn_Numpad(7));
+
+            Button num8 = new Button("8");
+            num8.setStyle("-fx-background-color: MediumSeaGreen");
+            num8.setMinSize(60, 45);
+            GridPane.setConstraints(num8, 3, 7);
+            num8.setOnAction(e -> btn_Numpad(8));
+
+            Button num9 = new Button("9");
+            num9.setStyle("-fx-background-color: MediumSeaGreen");
+            num9.setMinSize(60, 45);
+            GridPane.setConstraints(num9, 4, 7);
+            num9.setOnAction(e -> btn_Numpad(9));
+
+            Button num0 = new Button("0");
+            num0.setStyle("-fx-background-color: MediumSeaGreen");
+            num0.setMinSize(60, 45);
+            GridPane.setConstraints(num0, 3, 8);
+            num0.setOnAction(e -> btn_Numpad(0));
+
+            grid.getChildren().addAll(Scale,lblNumpad,lblNumber,Enter, num1, num2, num3, num4, num5, num6, num7, num8, num9, num0);
+            Scene scene = new Scene(grid, 1000, 1000);
+            stage.setScene(scene);
+            stage.show();
+
+
+
+
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
     private void btn_Scan() throws FileNotFoundException {
         Project project = new Project();
         String[][] newArray = project.parser();
@@ -280,6 +393,11 @@ public class Main extends Application{
         System.out.println(itemdescription.itemAmount[counter.count]);
         display = display +itemdescription.itemName[counter.count] +" "+ itemdescription.itemAmount[counter.count]+"\n";
 
+        if(isFirst)
+        {
+            loyalCustomer(1);
+        }
+
         changeStage(primaryStage, 1);
 
     }
@@ -287,6 +405,10 @@ public class Main extends Application{
     private void btn_Numpad(int number){//passes whatever number is pressed
         ItemID = ItemID + Integer.toString(number);
         try {
+            if(isFirst)
+            {
+                loyalCustomer(2);
+            }
             changeStage(primaryStage,2);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -294,11 +416,6 @@ public class Main extends Application{
     }
 
     private void btn_ITEM_ID() throws FileNotFoundException {
-        if(isFirst)
-        {
-            //method
-        }
-
         changeStage(primaryStage, 2);
     }
     private void btn_numpadEnter() throws FileNotFoundException {
