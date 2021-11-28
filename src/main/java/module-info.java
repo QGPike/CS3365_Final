af7b0923cfe8706f940f96d767fc7c8b53762be3
QGPike <69122709+QGPike@users.noreply.github.com>
2021-11-27 20:22:39 -0600

    File Architecture Push

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module sample.cs3365_final {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens sample.cs3365_final to javafx.fxml;
+    exports sample.cs3365_final;
+}
